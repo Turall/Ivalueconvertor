22aedbcb65e9c1767fa8a70897a80e2a375a777e
Tural Muradov <Tural_M@hotmail.com>
2018-03-08 21:24:28 +0400

    Add project files.

---
+++ WpfApp12/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp12
+{
+    public class Class1 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int val = System.Convert.ToInt32(value);
+            switch(val)
+            {
+                case 1: return "One";
+                case 2: return "Two";
+                case 3: return "Three";
+                case 4: return "Four";
+                case 5: return "Five";
+                case 6: return "Six";
+                case 7: return "Seven";
+                case 8: return "Eight";
+                case 9: return "Nine";
+                default: return "Error";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str = value.ToString();
+            switch (str)
+            {
+                case "One": return 1;
+                case "Two": return 2;
+                case "Three": return 3;
+                case "Four": return 4;
+                case "Five": return 5;
+                case "Six": return 6;
+                case "Seven": return 7;
+                case "Eight": return 8;
+                case "Nine": return 9;
+                default: return "Error";
+
+            }
+        }
+    }
+}
